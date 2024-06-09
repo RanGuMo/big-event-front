193839f8411caaeec313ac75586af6b6fef45247
MoRanGu <1126146235@qq.com>
2024-06-09 15:50:24 +0800

    feat(添加文章): ✨ 添加文章抽屉组件，富文本编辑器

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -124,6 +124,22 @@ const getArticles = async () => {
 
 getArticleCategoryList();
 getArticles();
+
+
+import { Plus } from '@element-plus/icons-vue'
+
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+//控制抽屉是否显示
+const visibleDrawer = ref(false)
+//添加表单数据模型
+const articleModel = ref({
+    title: '',
+    categoryId: '',
+    coverImg: '',
+    content:'',
+    state:''
+})
 </script>
 <template>
     <el-card class="page-container">
@@ -131,7 +147,7 @@ getArticles();
             <div class="header">
                 <span>文章管理</span>
                 <div class="extra">
-                    <el-button type="primary">添加文章</el-button>
+                    <el-button type="primary" @click="visibleDrawer=true">添加文章</el-button>
                 </div>
             </div>
         </template>
@@ -180,6 +196,45 @@ getArticles();
         <el-pagination v-model:current-page="pageNum" v-model:page-size="pageSize" :page-sizes="[3, 5 ,10, 15]"
             layout="jumper, total, sizes, prev, pager, next" background :total="total" @size-change="onSizeChange"
             @current-change="onCurrentChange" style="margin-top: 20px; justify-content: flex-end" />
+    <!-- 抽屉 -->
+    <el-drawer v-model="visibleDrawer" title="添加文章" direction="rtl" size="50%">
+            <!-- 添加文章表单 -->
+            <el-form :model="articleModel" label-width="100px" >
+                <el-form-item label="文章标题" >
+                    <el-input v-model="articleModel.title" placeholder="请输入标题"></el-input>
+                </el-form-item>
+                <el-form-item label="文章分类">
+                    <el-select placeholder="请选择" v-model="articleModel.categoryId">
+                        <el-option v-for="c in categorys" :key="c.id" :label="c.categoryName" :value="c.id">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="文章封面">
+
+                    <el-upload class="avatar-uploader" :auto-upload="false" :show-file-list="false">
+                        <img v-if="articleModel.coverImg" :src="articleModel.coverImg" class="avatar" />
+                        <el-icon v-else class="avatar-uploader-icon">
+                            <Plus />
+                        </el-icon>
+                    </el-upload>
+                </el-form-item>
+                <el-form-item label="文章内容">
+                    <div class="editor">
+                        <quill-editor
+              theme="snow"
+              v-model:content="articleModel.content"
+              contentType="html"
+              >
+</quill-editor>
+                    </div>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary">发布</el-button>
+                    <el-button type="info">草稿</el-button>
+                </el-form-item>
+            </el-form>
+        </el-drawer>
+    
     </el-card>
 </template>
 <style lang="scss" scoped>
@@ -200,5 +255,44 @@ getArticles();
 
 .demo-form-inline .el-select {
   --el-select-width: 220px;
+}
+
+
+/* 抽屉样式 */
+.avatar-uploader {
+    :deep() {
+        .avatar {
+            width: 178px;
+            height: 178px;
+            display: block;
+        }
+
+        .el-upload {
+            border: 1px dashed var(--el-border-color);
+            border-radius: 6px;
+            cursor: pointer;
+            position: relative;
+            overflow: hidden;
+            transition: var(--el-transition-duration-fast);
+        }
+
+        .el-upload:hover {
+            border-color: var(--el-color-primary);
+        }
+
+        .el-icon.avatar-uploader-icon {
+            font-size: 28px;
+            color: #8c939d;
+            width: 178px;
+            height: 178px;
+            text-align: center;
+        }
+    }
+}
+.editor {
+  width: 100%;
+  :deep(.ql-editor) {
+    min-height: 200px;
+  }
 }
 </style>
